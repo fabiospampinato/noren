5e05e6f22ed32ce5b008e45a7c6382b47c61bdc7
Fabio Spampinato <spampinabio@gmail.com>
2023-03-04 22:21:20 +0000

    Improved basicAuth performance

--- src/middlewares/basic_auth.ts
+++ src/middlewares/basic_auth.ts
@@ -8,21 +8,33 @@ import type {RequestHandler} from '~/server/types';
 
 const basicAuth = ( options: { users: { username: string, password: string }[], realm?: string } ): RequestHandler => {
 
-  const users = options.users;
+  const users = new Map<string, string> ();
   const realm = options.realm || 'Protected Area';
 
   return async ( req, res ) => {
+
+    if ( !users.size ) { // Initing the users map //TODO: This should really be outside of the middleware though...
+
+      for ( let i = 0, l = options.users.length; i < l; i++ ) {
+
+        const user = options.users[i];
+        const hash = await sha256 ( user.password );
+
+        users.set ( user.username, hash );
+
+      }
+
+    }
 
     const credentials = req.credentials;
 
     if ( credentials ) {
 
       const {username, password} = credentials;
-      const user = users.find ( user => user.username === username );
+      const hash1 = users.get ( username );
 
-      if ( user ) {
+      if ( hash1 ) {
 
-        const hash1 = await sha256 ( user.password );
         const hash2 = await sha256 ( password );
 
         if ( hash1 === hash2 ) {
